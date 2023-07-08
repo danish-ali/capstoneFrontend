0e452eb503703bee0237e77613dfcca5454f8b50
Syed Danish Ali <danish_ali49@yahoo.com>
2023-07-08 13:22:28 -0400

    updated  styling of filter component

--- src/FilterComponent.js
+++ src/FilterComponent.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const FilterComponent = ({ onFilter }) => {
   const [filterData, setFilterData] = useState({
-    startDate: '',
-    endDate: '',
+    startDate: null,
+    endDate: null,
     newsSource: '',
     compoundValue: ''
   });
@@ -13,14 +15,18 @@ const FilterComponent = ({ onFilter }) => {
     setFilterData((prevState) => ({ ...prevState, [name]: value }));
   };
 
+  const handleDateChange = (name, date) => {
+    setFilterData((prevState) => ({ ...prevState, [name]: date }));
+  };
+
   const handleFilter = () => {
     onFilter(filterData);
   };
 
   const handleReset = () => {
     setFilterData({
-      startDate: '',
-      endDate: '',
+      startDate: null,
+      endDate: null,
       newsSource: '',
       compoundValue: ''
     });
@@ -28,26 +34,50 @@ const FilterComponent = ({ onFilter }) => {
   };
 
   return (
-    <div>
-      <h3>Filter</h3>
-      <div>
+    <div className="filter-container">
+      <h3 className="filter-heading">Filter</h3>
+      <div className="filter-row">
         <label>Start Date:</label>
-        <input type="text" name="startDate" value={filterData.startDate} onChange={handleInputChange} />
+        <DatePicker
+          selected={filterData.startDate}
+          onChange={(date) => handleDateChange('startDate', date)}
+          className="filter-datepicker"
+        />
       </div>
-      <div>
+      <div className="filter-row">
         <label>End Date:</label>
-        <input type="text" name="endDate" value={filterData.endDate} onChange={handleInputChange} />
+        <DatePicker
+          selected={filterData.endDate}
+          onChange={(date) => handleDateChange('endDate', date)}
+          className="filter-datepicker"
+        />
       </div>
-      <div>
+      <div className="filter-row">
         <label>News Source:</label>
-        <input type="text" name="newsSource" value={filterData.newsSource} onChange={handleInputChange} />
+        <input
+          type="text"
+          name="newsSource"
+          value={filterData.newsSource}
+          onChange={handleInputChange}
+        />
       </div>
-      <div>
+      <div className="filter-row">
         <label>Compound Value:</label>
-        <input type="text" name="compoundValue" value={filterData.compoundValue} onChange={handleInputChange} />
+        <input
+          type="text"
+          name="compoundValue"
+          value={filterData.compoundValue}
+          onChange={handleInputChange}
+        />
       </div>
-      <button onClick={handleFilter}>Apply Filter</button>
-      <button onClick={handleReset}>Reset</button>
+      <div className="filter-button-row">
+        <button className="filter-button apply-button" onClick={handleFilter}>
+          Apply Filter
+        </button>
+        <button className="filter-button reset-button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
