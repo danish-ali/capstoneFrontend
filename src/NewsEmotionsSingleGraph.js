b82c1a1ec5569ff71d571b3b68a1523403ed9cb6
Syed Danish Ali <danish_ali49@yahoo.com>
2023-07-09 00:44:14 -0400

    updated pie chart

--- src/NewsEmotionsSingleGraph.js
+++ src/NewsEmotionsSingleGraph.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart, CategoryScale, LinearScale, LineController, PointElement, LineElement, TimeScale, TimeSeriesScale } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 import dayjs from 'dayjs';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import 'chartjs-adapter-dayjs';
 import FilterComponent from './FilterComponent';
-
-Chart.register(CategoryScale, LinearScale, LineController, PointElement, LineElement, TimeScale, TimeSeriesScale, ChartDataLabels);
+import 'chartjs-adapter-dayjs';
 
 const NewsEmotionsSingleGraph = () => {
   const [data, setData] = useState(null);
@@ -34,11 +31,11 @@ const NewsEmotionsSingleGraph = () => {
         compoundValue: filterData.compoundValue || '',
         newsSources: encodeURIComponent(JSON.stringify(filterData.newsSources))
       };
-  
+
       const queryString = Object.keys(queryParams)
         .map(key => `${key}=${queryParams[key]}`)
         .join('&');
-  
+
       const url = `http://localhost:5000/getNewsEmotionsSingleGraphDB?${queryString}`;
       const response = await fetch(url);
       const responseData = await response.json();
@@ -51,96 +48,65 @@ const NewsEmotionsSingleGraph = () => {
   const createChartData = () => {
     if (!filteredData) return null;
 
-    const chartData = {
-      labels: [], // Dates
-      datasets: [] // Emotion datasets for each news source
-    };
+    const chartData = {};
 
     for (const newsSource in filteredData) {
       const emotionData = filteredData[newsSource];
+      const positiveCount = emotionData.pos ? emotionData.pos.reduce((count, value) => count + value, 0) : 0;
+      const negativeCount = emotionData.neg ? emotionData.neg.reduce((count, value) => count + value, 0) : 0;
 
-      const emotionDataset = {
-        label: newsSource,
-        data: emotionData.compound,
-        fill: false,
-        borderColor: getRandomColor(),
-        tension: 0.1
+
+      chartData[newsSource] = {
+        labels: ['Positive', 'Negative'],
+        datasets: [
+          {
+            data: [positiveCount, negativeCount],
+            backgroundColor: ['#4CAF50', '#F44336'],
+            hoverOffset: 4,
+          },
+        ],
       };
-
-      chartData.datasets.push(emotionDataset);
     }
-
-    const firstNewsSource = Object.keys(filteredData)[0];
-    const firstEmotionData = filteredData[firstNewsSource];
-    chartData.labels = firstEmotionData.date.map(date => dayjs(date).format('YYYY-MM-DD'));
 
     return chartData;
   };
 
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
   const options = {
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'day',
-          displayFormats: {
-            day: 'YYYY-MM-DD'
-          }
-        },
-        ticks: {
-          source: 'labels'
-        }
-      },
-      y: {
-        beginAtZero: false,
-        min: -1,
-        max: 1,
-        ticks: {
-          stepSize: 0.2,
-          callback: (value) => {
-            return value.toFixed(2);
-          }
-        }
-      }
-    },
     plugins: {
-      datalabels: {
+      title: {
         display: true,
-        color: 'white'
-      }
-    }
+        text: 'Sentiment Distribution',
+        font: { weight: 'bold' },
+      },
+      legend: {
+        position: 'bottom',
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const label = context.label;
+            const value = context.raw;
+            return `${label}: ${value}`;
+          },
+        },
+      },
+    },
   };
 
   return (
     <div>
       <FilterComponent onFilter={handleFilter} />
-      {filteredData && Object.entries(filteredData).map(([newsSource, emotionData]) => (
-        <div key={newsSource}>
-          <h2>{newsSource}</h2>
-          <Line
-            data={{
-              labels: emotionData.date.map(date => dayjs(date).format('YYYY-MM-DD')),
-              datasets: [{
-                label: newsSource,
-                data: emotionData.compound,
-                fill: false,
-                borderColor: getRandomColor(),
-                tension: 0.1
-              }]
-            }}
-            options={options}
-          />
-        </div>
-      ))}
+      {filteredData &&
+        Object.entries(filteredData).map(([newsSource, emotionData]) => (
+          <div key={newsSource}>
+            <h2>{newsSource}</h2>
+            {emotionData && (
+              <div style={{ maxWidth: '400px' }}>
+                <Pie data={createChartData()[newsSource]} options={options} />
+              </div>
+            )}
+          </div>
+        ))}
     </div>
   );
 };
