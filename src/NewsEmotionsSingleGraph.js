b318ea40d3e370ce824041db606a76bf36c62ed7
Syed Danish Ali <danish_ali49@yahoo.com>
2023-07-08 20:50:50 -0400

    working code with filter

--- src/NewsEmotionsSingleGraph.js
+++ src/NewsEmotionsSingleGraph.js
@@ -13,34 +13,28 @@ const NewsEmotionsSingleGraph = () => {
   const [filteredData, setFilteredData] = useState(null);
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/getNewsEmotionsSingleGraphDB');
+        const responseData = await response.json();
+        setData(responseData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
     fetchData();
   }, []);
 
-  const fetchData = async () => {
+  const handleFilter = async (filterData) => {
     try {
-      const response = await fetch('http://localhost:5000/getNewsEmotionsSingleGraphDB');
+      const params = new URLSearchParams(filterData).toString();
+      const response = await fetch(`http://localhost:5000/getNewsEmotionsSingleGraphDB?${params}`);
       const responseData = await response.json();
-      setData(responseData);
       setFilteredData(responseData);
     } catch (error) {
       console.error('Error:', error);
     }
-  };
-
-  const handleFilter = (filterData) => {
-    // Apply the filtering logic based on the filterData
-    // Update the filteredData state with the filtered data
-
-    // Example filtering logic:
-    const filtered = data.filter((item) => {
-      const matchDate = filterData.date ? dayjs(item.date).format('YYYY-MM-DD') === filterData.date : true;
-      const matchNewsSource = filterData.newsSource ? item.newsSource === filterData.newsSource : true;
-      const matchCompound = filterData.compound ? item.compound === parseFloat(filterData.compound) : true;
-
-      return matchDate && matchNewsSource && matchCompound;
-    });
-
-    setFilteredData(filtered);
   };
 
   const createChartData = () => {
@@ -67,7 +61,7 @@ const NewsEmotionsSingleGraph = () => {
 
     const firstNewsSource = Object.keys(filteredData)[0];
     const firstEmotionData = filteredData[firstNewsSource];
-    chartData.labels = firstEmotionData.date.map((date) => dayjs(date).format('YYYY-MM-DD'));
+    chartData.labels = firstEmotionData.date.map(date => dayjs(date).format('YYYY-MM-DD'));
 
     return chartData;
   };
@@ -96,18 +90,13 @@ const NewsEmotionsSingleGraph = () => {
         }
       },
       y: {
+        beginAtZero: false,
         min: -1,
         max: 1,
         ticks: {
           stepSize: 0.2,
           callback: (value) => {
-            if (value === 0) {
-              return 'Neutral';
-            } else if (value < 0) {
-              return 'Negative';
-            } else {
-              return 'Positive';
-            }
+            return value.toFixed(2);
           }
         }
       }
@@ -123,29 +112,24 @@ const NewsEmotionsSingleGraph = () => {
   return (
     <div>
       <FilterComponent onFilter={handleFilter} />
-
-      {/* Render the chart using the filteredData */}
-      {filteredData &&
-        Object.entries(filteredData).map(([newsSource, emotionData]) => (
-          <div key={newsSource}>
-            <h2>{newsSource}</h2>
-            <Line
-              data={{
-                labels: emotionData.date.map((date) => dayjs(date).format('YYYY-MM-DD')),
-                datasets: [
-                  {
-                    label: newsSource,
-                    data: emotionData.compound,
-                    fill: false,
-                    borderColor: getRandomColor(),
-                    tension: 0.1
-                  }
-                ]
-              }}
-              options={options}
-            />
-          </div>
-        ))}
+      {filteredData && Object.entries(filteredData).map(([newsSource, emotionData]) => (
+        <div key={newsSource}>
+          <h2>{newsSource}</h2>
+          <Line
+            data={{
+              labels: emotionData.date.map(date => dayjs(date).format('YYYY-MM-DD')),
+              datasets: [{
+                label: newsSource,
+                data: emotionData.compound,
+                fill: false,
+                borderColor: getRandomColor(),
+                tension: 0.1
+              }]
+            }}
+            options={options}
+          />
+        </div>
+      ))}
     </div>
   );
 };
